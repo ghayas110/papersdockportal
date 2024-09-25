089ced08cedd43aa0633640af6bcc0ed02a54f05
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2024-09-25 19:50:32 +0500

    push

--- src/app/viewNotes/[paperid]/page.tsx
+++ src/app/viewNotes/[paperid]/page.tsx
@@ -5,11 +5,14 @@ import { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import Header2 from '@/sections/Header2';
+import { LuGalleryHorizontal } from 'react-icons/lu';
 
 interface Note {
   note_id: string;
   note_url: string;
   created_at: string;
+  name: string;
+  image: string;
 }
 
 interface ViewWebNotesProps {
@@ -73,42 +76,41 @@ const ViewWebNotes: React.FC<ViewWebNotesProps> = ({ params }) => {
     >
       <Header2 />
       <div className="container mx-auto p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {notes.map((note) => {
-          const fileName = formatFileName(note.note_url); // Format the file name
-          return (
-            <div
-              key={note.note_id}
-              className="flex flex-col items-center justify-center bg-[#010E24] rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-              style={{ width: '240px', height: '250px', cursor: 'pointer' }}
-              onClick={() => handleViewNote(note)}
-            >
-              {/* White portion on top */}
+        {notes.length > 0 ? (
+          notes.map((note) => {
+            const fileName = formatFileName(note.note_url); // Format the file name
+            return (
               <div
-                className="w-full bg-white text-center py-2"
-                style={{ borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}
+                key={note.note_id}
+                className="flex flex-col items-center justify-center bg-[#010E24] rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                style={{ width: '240px', height: '250px', cursor: 'pointer' }}
+                onClick={() => handleViewNote(note)}
               >
-                <h3 style={{ fontWeight: 'bold', fontSize: '14px', margin: '0', color: '#010E24' }}>
-                {(fileName).toUpperCase()}
-                </h3>
-              </div>
+                {/* White portion on top */}
+                <div
+                  className="w-full bg-white text-center py-2"
+                  style={{ borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}
+                >
+                  <h3 style={{ fontWeight: 'bold', fontSize: '14px', margin: '0', color: '#010E24' }}>
+                    {(note.name).toUpperCase()}
+                  </h3>
+                </div>
 
-              {/* Main Content */}
-              <div
-                className="flex-1 flex flex-col items-center justify-center"
-                style={{  textAlign: 'center'  }}
-              >
-                         <h1 style={{ fontWeight: 'bold', fontSize: '24px', color: 'white' }}>    Papers Dock</h1>
-                <h1 style={{ fontWeight: 'bold', fontSize: '24px', color: 'yellow', margin: '8px 0'}}>{(fileName).toUpperCase()}</h1>
-                <p style={{ color: 'yellow', fontSize: '16px', margin: '8px 0', fontWeight: 'bold' }}>
-              {(paperNo).toUpperCase()}
-                </p>
-                <p style={{ fontWeight: 'bold', fontSize: '16px', color: 'white' }}>
-                {moment(note.created_at).format('MMMM Do YYYY')}
-                </p>
+                {/* Main Content */}
+                <div
+                  className="flex-1 flex flex-col items-center justify-center"
+                  style={{ textAlign: 'center' }}
+                >
+                  <img src={`https://be.papersdock.com${note.image}`} alt="PDF Icon" style={{ width: '100px', height: '100px' }} />
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="col-span-1 md:col-span-2 lg:col-span-4 text-center text-white">
+            <h2>No Notes Found</h2>
+          </div>
+        )}
 
         {/* View Note Modal */}
         <Modal
